e116669a9901c37e992e4ce1fb758d5cc4345241
elisa lee <elisa.my.lee@gmail.com>
2016-10-23 18:29:09 -0500

    refactor server code

--- routes/database.js
+++ routes/database.js
@@ -2,55 +2,62 @@ var router = require('express').Router();
 var pg = require('pg');
 
 var config = {
-  database: 'rho'
+  database: 'rho',
 };
 
-// initialize the database connection pool
 var pool = new pg.Pool(config);
 
-router.get('/', function(req, res){
-  pool.connect(function(err, client, done) {
-    if (err) {
-      console.log('Error connecting to the DB', err);
-      res.sendStatus(500);
-      done();
-      return;
-    }
-    client.query('SELECT * FROM favorites;', function(err, result){
-      done();
+router.get('/', function (req, res) {
+  pool.connect(function (err, client, done) {
+    try {
       if (err) {
-        console.log('Error querying the DB', err);
+        console.log('Error connecting to the database', err);
         res.sendStatus(500);
         return;
       }
 
-      console.log('Got rows from the DB:', result.rows);
-      res.send(result.rows);
-    });
+      client.query('SELECT * FROM favorites;',
+        function (err, result) {
+          if (err) {
+            console.log('Error querying the database', err);
+            res.sendStatus(500);
+            return;
+          }
+
+          console.log('Got rows from the database: ', result.rows);
+          res.send(result.rows);
+        });
+
+    } finally {
+      done();
+    }
   });
 });
 
-router.post('/', function(req, res){
-  pool.connect(function(err, client, done){
-    if (err) {
-      console.log('Error connecting the DB', err);
-      res.sendStatus(500);
-      done();
-      return;
-    }
-
-    client.query('INSERT INTO favorites (url, comment) VALUES ($1, $2) returning *;',
-    [req.body.url, req.body.comment],
-    function(err, result){
-      done();
+router.post('/', function (req, res) {
+  pool.connect(function (err, client, done) {
+    try {
       if (err) {
-        console.log('Error querying the DB', err);
+        console.log('Error connecting to the database', err);
         res.sendStatus(500);
         return;
       }
-      console.log('Got rows from the DB:', result.rows);
-      res.send(result.rows);
-    });
+
+      client.query('INSERT INTO favorites (url, comment) VALUES ($1, $2) returning *;',
+        [req.body.url, req.body.comment],
+        function (err, result) {
+          if (err) {
+            console.log('Error querying the database', err);
+            res.sendStatus(500);
+            return;
+          }
+
+          console.log('Got rows from the database: ', result.rows);
+          res.send(result.rows);
+        });
+    } finally {
+      done();
+    }
   });
 });
 
